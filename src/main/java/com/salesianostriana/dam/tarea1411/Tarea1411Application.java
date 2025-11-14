236055695b6e85d5ac70d837cd8baccdd7cc8574
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2025-11-14 09:17:01 +0100

    Proyecto inicializado

---
+++ src/main/java/com/salesianostriana/dam/tarea1411/Tarea1411Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.tarea1411;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea1411Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea1411Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianostriana/dam/tarea1411/Tarea1411ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.tarea1411;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea1411ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
